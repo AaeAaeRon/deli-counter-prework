4f4b3b3b35568317ff42fd48cde62dcd8a9824bc
Aaron B <abriscoe2008@gmail.com>
2019-05-23 21:15:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,5 +15,5 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  puts "Welcome, #{name} you are #{katz_deli.length} in lline."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end 
