d2bca8df443e34cd9d857f13644b0bf71b80ef34
Aaron B <abriscoe2008@gmail.com>
2019-05-23 21:17:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,17 @@ def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end 
+
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "BBBBBBBBB"
+  else 
+  end 
+end 
+
+
+
+
+
+
